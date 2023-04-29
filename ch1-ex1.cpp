e444375fc538e7faf164b8c948aff32f93c5e307
Johnson Umeike <umeikejc@gmail.com>
2023-04-28 19:58:00 -0500

    chp1-ex1

---
+++ ch1-ex1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout << "Oh what" << endl;
+    cout << "a happy day!\n Oh yes," << endl;
+    cout << "what a happy day!" << endl;
+    return 0;
+}
+
